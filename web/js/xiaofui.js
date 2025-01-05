e808e67335cbd3e46abbebdbf4317a64f7b561a7
Yuki Goh <xiaofui233134@dasein.edu.my>
2025-01-05 12:13:27 +0800

    commit

--- web/js/xiaofui.js
+++ web/js/xiaofui.js
@@ -14,3 +14,19 @@ alignElements.forEach(element => {
     });
   });
 
+/*adoptionpage*/
+/*hover dog*/
+const dogElements = document.querySelectorAll ('.dog');
+dogElements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+      });
+    
+      //mouse out 
+      element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+      });
+    });
