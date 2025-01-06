0c9b88331ec9c5f9f289512bccac44500471755b
Rebecca <rebecca233142@dasein.edu.my>
2025-01-07 02:54:46 +0800

    adjustment

---
+++ web/js/rebecca.js
@@ -0,0 +1,74 @@
+const dogElements = document.querySelectorAll('.dog');
+dogElements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+    });
+
+    // mouse out 
+    element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+    });
+});
+
+const dog2Elements = document.querySelectorAll('.dog2');
+dog2Elements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+    });
+
+    // mouse out 
+    element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+    });
+});
+
+const dog3Elements = document.querySelectorAll('.dog3');
+dog3Elements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+    });
+
+    // mouse out 
+    element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+    });
+});
+
+const catElements = document.querySelectorAll('.cat');
+catElements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+    });
+
+    // mouse out 
+    element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+    });
+});
+
+const othersElements = document.querySelectorAll('.others');
+othersElements.forEach(element => {
+    // mouse over
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'scale(1.1)';
+        element.style.cursor = 'pointer';
+        element.style.transition = '0.3s ease-in-out';
+    });
+
+    // mouse out 
+    element.addEventListener('mouseout', () => {
+        element.style.transform = 'scale(1)';
+    });
+});
